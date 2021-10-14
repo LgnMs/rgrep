314c8134f42bd03431f316ce8292c7ac510d0d13
LGN <792841450@qq.com>
2021-10-14 17:45:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# rgrep
+
+Hello 1
+实现一个简单的grep
+
+## 示例
+
+```s
+$ rgrep Hello a.txt
+55: Hello world. This is an exmaple text
+```
+
+## 思考
+
+1.先梳理整个整个项目的流程
+2.定义项目的接口
+3.测试接口是否稳定或者逻辑是否有问题
+4.实现接口功能
---
+++ src/main.rs
@@ -0,0 +1,127 @@
+use std::{path::Path, path::PathBuf, str::FromStr};
+use regex::Regex;
+use anyhow::*;
+use colored::Colorize;
+use clap::{App, Arg};
+use std::fs;
+use globset::{Glob, GlobMatcher};
+
+trait Rgrep<T> {
+    fn search(&mut self, str: &str) -> &Self;
+    fn read(&mut self) -> &mut Self;
+    fn print_result(&self);
+}
+
+#[derive(Default, Debug)]
+struct FileGrep {
+    value: String,
+    path: PathBuf,
+    file_name: String,
+    result: Vec<Vec<String>>,
+}
+
+impl FileGrep {
+    fn new(path: PathBuf, file_name: String) -> Self {
+        Self {
+            path,
+            file_name,
+            ..Default::default()
+        }
+    }
+}
+
+impl Rgrep<FileGrep> for FileGrep {
+    fn search(&mut self, str: &str) -> &Self {
+        let mut result = Vec::new();
+
+        for (row, line) in self.value.lines().enumerate() {
+            if let Some(mat) = Regex::new(str).unwrap().find(line) {
+                let s_i = mat.start();
+                let e_i = mat.end();
+                let line_res = vec![
+                    format!("{}:{}", row + 1, s_i),
+                    line[..s_i].to_string(),
+                    line[s_i..e_i].to_string(),
+                    line[e_i..].to_string()
+                ];
+                result.push(line_res);
+            }
+        }
+        self.result = result;
+        self
+    }
+    fn print_result(&self) {
+        if !self.result.is_empty() {
+            println!("{}", self.file_name.yellow());
+        }
+        for line in self.result.iter() {
+            println!("{} {}{}{}", line[0].as_str().blue(), line[1], line[2].red(), line[3]);
+        }
+    }
+    fn read(&mut self) -> &mut Self {
+        let content = fs::read_to_string(self.path.clone()).unwrap();
+        self.value = content;
+        self
+    }
+}
+
+impl FromStr for FileGrep {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(FileGrep { value: s.into(), ..Default::default() })
+    }
+}
+
+fn visit_dirs(dir: &Path, match_str: &str, glob: &GlobMatcher) -> Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, match_str, glob)?;
+            } else {
+                if glob.is_match(entry.path()) {
+                    FileGrep::new(entry.path(), entry.file_name().into_string().unwrap())
+                        .read()
+                        .search(match_str)
+                        .print_result();
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let matches = App::new("rgrep")
+        .version("v0.1")
+        .arg(
+            Arg::new("match_str")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::new("file_path")
+                .takes_value(true)
+        )
+        .get_matches();
+    let match_str = matches.value_of("match_str").unwrap();
+    let file_path = matches.value_of("file_path").unwrap();
+    let glob = Glob::new(file_path)?.compile_matcher();
+    visit_dirs(Path::new("."), match_str, &glob)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basi_interface() -> Result<()> {
+        FileGrep::new("./a.md".into(), "a.md".into())
+            .read()
+            .search("Hel[^\\s]+")
+            .print_result();
+        // let file_grep  = file_str.search("Hello");
+        Ok(())
+    }
+}
